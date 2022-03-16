623eaa9b224245604c0bf4ed7f4412ca9d870e6f
Corey <corey.hampton@student.awesomeinc.org>
2022-03-16 20:35:37 +0000

    Update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { ReactSVG } from 'react-svg'
+
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link, Outlet } from 'react-router-dom';
 import './App.css';
 
@@ -10,17 +11,18 @@ function App() {
   return (
     <div>
       <Navbar bg="dark" variant="dark" expand="lg">
-      <Container>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/"><h1>HOME</h1></Nav.Link>
-            <Nav.Link href="/menu"><h1>MENU</h1></Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-    <Outlet />
+        <Container>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/"><h1>HOME</h1></Nav.Link>
+              <Nav.Link href="/menu"><h1>MENU</h1></Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+            <ReactSVG src="img/OmniEats-White.svg" wrapper="svg" className='logo'/>
+        </Container>
+      </Navbar>
+      <Outlet />
     </div>
   );
 }
--- src/routes/Menu.js
+++ src/routes/Menu.js
@@ -3,19 +3,21 @@ import { getLocalStorage, setLocalStorage } from '../utils/localStorage';
 import { getData } from '../utils/data';
 
 export default function Menu() {
-  const URL = 'https://sheltered-refuge-85246.herokuapp.com/api/json';
-  const [items, setItems] = useState([]);
-  
-  useEffect(() => {
-    let data = getLocalStorage();
-    if (data.length > 0) {
-      setItems(data);
-    } else {
-      getData(URL)
-        .then((data) => {
-          setItems(data);
-          setLocalStorage(data);
-        })
-    }
-  }, []);
+    return (
+        <div>
+            <Navbar bg="dark" variant="dark" expand="lg">
+                <Container>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="/"><h1>HOME</h1></Nav.Link>
+                            <Nav.Link href="/menu"><h1>MENU</h1></Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                    <ReactSVG src="img/OmniEats-White.svg" wrapper="svg" className='logo' />
+                </Container>
+            </Navbar>
+            <Outlet />
+        </div>
+    );
 }
